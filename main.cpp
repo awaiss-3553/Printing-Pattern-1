52640b155558952e75b45629d8f06729bdb951ac
Muhammad Awais <awaiss3553@gmail.com>
2026-04-05 12:29:52 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"enter number ";
+    cin>>n;
+        for(int i; i<=n; i++){ // for lines
+                int m=10;
+            for(int j=1; j<=m; j++){ // for output
+               cout<<j;
+            }
+      cout<<endl;
+        }
+
+}
